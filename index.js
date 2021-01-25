025587c5864e9e59f1f2e0ccdfac8ba851fc7e6d
ShikharSingh98 <shikharsingh98@gmail.com>
2021-01-26 01:25:11 +0530

    Implemented edit task functionality

--- index.js
+++ index.js
@@ -35,49 +35,6 @@ date.append(moment().format('D'), superscript);
 day.textContent = moment().format('dddd');
 monthYear.textContent = moment().format('MMMM YYYY');
 
-// Render task
-function renderTask(task) {
-  const li = document.createElement('li');
-  li.classList.add('task');
-  li.dataset.taskId = task.id;
-
-  const div = document.createElement('div');
-
-  const checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox');
-  if (task.completed) {
-    checkbox.checked = true;
-  }
-  checkbox.classList.add('task-checkbox');
-  checkbox.addEventListener('click', changeTaskStatus);
-
-  const span = document.createElement('span');
-  span.classList.add('task-text');
-  if (task.completed) {
-    span.classList.add('task-completed');
-  }
-  span.innerText = task.task;
-
-  div.append(checkbox, span);
-
-  const icon = document.createElement('i');
-  icon.classList.add('task-delete-icon', 'fas', 'fa-trash');
-  icon.addEventListener('click', deleteTask);
-
-  li.append(div, icon);
-
-  tasksList.append(li);
-}
-
-//Render all task from local storage
-function renderAllTasksFromLocal() {
-  let tasks = [];
-  if (localStorage.getItem('tasks')) {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-  tasks.forEach((task) => renderTask(task));
-}
-
 //Save task to local storage
 function saveTaskToLocal(task) {
   let tasks = [];
@@ -111,7 +68,21 @@ function changeTaskStatusInLocal(taskId, status) {
   localStorage.setItem('tasks', JSON.stringify(newTasks));
 }
 
+// Edit task in Local Storage
+
+function editTaskInLocalStorage(taskId, taskData) {
+  let tasks = [];
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  const newTasks = tasks.map((task) =>
+    task.id === taskId ? { ...task, task: taskData } : task
+  );
+  localStorage.setItem('tasks', JSON.stringify(newTasks));
+}
+
 // Set tasks left counter
+
 let tasksCount = 0;
 const tasks = JSON.parse(localStorage.getItem('tasks'));
 if (tasks) {
@@ -137,11 +108,42 @@ function decrementTasksCount() {
   tasksLeft.innerText = tasksCount;
 }
 
+//Edit Task
+
+function editTask(event) {
+  event.preventDefault();
+  const editForm = event.target;
+  const task = editForm.parentElement;
+  const taskInput = editForm.children[0];
+  const editButton = editForm.children[1];
+  const checkbox = editForm.previousElementSibling;
+  if (editButton.classList.contains('fa-edit')) {
+    taskInput.disabled = false;
+    taskInput.focus();
+    taskInput.style.backgroundColor = '#dfdfdf';
+    editButton.classList.remove('fa-edit');
+    editButton.classList.add('fa-save');
+    checkbox.disabled = true;
+  } else {
+    if (taskInput.value.trim()) {
+      editTaskInLocalStorage(
+        parseInt(task.dataset.taskId),
+        taskInput.value.trim()
+      );
+      editButton.classList.remove('fa-save');
+      editButton.classList.add('fa-edit');
+      taskInput.style.backgroundColor = 'white';
+      taskInput.disabled = true;
+      checkbox.disabled = false;
+    }
+  }
+}
+
 //Delete Task
 
 function deleteTask(event) {
   const task = event.target.parentElement;
-  const checkbox = task.children[0].children[0];
+  const checkbox = task.children[0];
   //If the checkbox is not checked then only decrement count
   if (!checkbox.checked) {
     decrementTasksCount();
@@ -155,19 +157,88 @@ function deleteTask(event) {
 
 function changeTaskStatus(event) {
   const checkbox = event.target;
-  const span = checkbox.nextElementSibling;
-  const taskId = parseInt(
-    event.target.parentElement.parentElement.dataset.taskId
-  );
+  const input = checkbox.nextElementSibling.children[0];
+  const editButton = checkbox.nextElementSibling.children[1];
+  const taskId = parseInt(checkbox.parentElement.dataset.taskId);
   if (checkbox.checked) {
-    span.classList.add('task-completed');
+    input.classList.add('task-completed');
     changeTaskStatusInLocal(taskId, true);
     decrementTasksCount();
+    editButton.style.display = 'none';
   } else {
-    span.classList.remove('task-completed');
+    input.classList.remove('task-completed');
     changeTaskStatusInLocal(taskId, false);
     incrementTasksCount();
+    editButton.style.display = 'inline';
   }
+}
+
+// On Input Change
+
+function onInputChange(event) {
+  const taskInput = event.target;
+  const saveIcon = taskInput.nextElementSibling;
+  if (taskInput.value.trim()) {
+    taskInput.style.backgroundColor = '#dfdfdf';
+    saveIcon.style.display = 'inline';
+  } else {
+    taskInput.placeholder = 'Task cannot be empty 😬';
+    saveIcon.style.display = 'none';
+  }
+}
+
+// Render task
+function renderTask(task) {
+  const li = document.createElement('li');
+  li.classList.add('task');
+  li.dataset.taskId = task.id;
+
+  const checkbox = document.createElement('input');
+  checkbox.setAttribute('type', 'checkbox');
+  if (task.completed) {
+    checkbox.checked = true;
+  }
+  checkbox.classList.add('task-checkbox');
+  checkbox.addEventListener('click', changeTaskStatus);
+
+  const editForm = document.createElement('form');
+  editForm.addEventListener('submit', editTask);
+  editForm.classList.add('task-edit-form');
+
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.classList.add('task-input');
+  input.addEventListener('keyup', onInputChange);
+  input.disabled = true;
+  if (task.completed) {
+    input.classList.add('task-completed');
+  }
+  input.value = task.task;
+
+  const editButton = document.createElement('button');
+  editButton.classList.add('task-icon', 'fas', 'fa-edit');
+  if (task.completed) {
+    editButton.style.display = 'none';
+  }
+
+  editForm.append(input, editButton);
+
+  const deleteIcon = document.createElement('i');
+  deleteIcon.classList.add('task-icon', 'fas', 'fa-trash');
+  deleteIcon.addEventListener('click', deleteTask);
+
+  li.append(checkbox, editForm, deleteIcon);
+
+  tasksList.append(li);
+}
+
+//Render all task from local storage
+function renderAllTasksFromLocal() {
+  let tasks = [];
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.forEach((task) => renderTask(task));
 }
 
 // Create Task
@@ -175,7 +246,7 @@ function changeTaskStatus(event) {
 function createTask(task) {
   const taskObj = {
     id: Date.now(),
-    task: task,
+    task: task.trim(),
     completed: false,
   };
   saveTaskToLocal(taskObj);
